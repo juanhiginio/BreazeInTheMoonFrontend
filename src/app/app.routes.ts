43cd221319d593f3e3f6454c842e82648b9754a9
juanhiginio <juanhiginio06@gmail.com>
2025-04-16 22:28:09 -0500

    Configure routes for login and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},   
+    {path: 'login', component: LoginComponent},
+];
