68b4533138f507a6cf4338ad0d11bb526eb0fa35
elcesar172006 <cesard.ariasp@autonoma.edu.co>
2025-04-28 17:05:57 -0500

    login
    solo falta cambiar imagenes y titulo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule]
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+
+  loginWith(provider: string) {
+    console.log(`Logging in with ${provider}`);
+    // Aquí puedes implementar la lógica para iniciar sesión con el proveedor seleccionado
+  }
+
+  register() {
+    console.log('Redirecting to registration page');
+    // Aquí puedes implementar la lógica para redirigir a la página de registro
+  }
 }
